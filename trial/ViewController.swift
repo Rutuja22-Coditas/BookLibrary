3e788843ef3ea6f20e6f53e886d157c4e4137157
Rutuja Hulsure <rutuja.hulsure@coditas.com>
2021-10-04 11:52:53 +0530

    Initial Commit

---
+++ trial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trial
+//
+//  Created by Coditas on 04/10/21.
+//  Copyright © 2021 Coditas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
